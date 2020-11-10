20c7d7fe58d662ae6b328b84063b502c3fc641a5
William Parra <w3webservices.net@gmail.com>
2020-11-10 17:51:57 -0400

    primer commit de la app

---
+++ index.js
@@ -0,0 +1,26 @@
+const { ApolloServer } = require('Apollo-Server')
+const mongoose = require('mongoose')
+
+const resolvers = require('./graphql/resolvers')
+const { MONGODB } = require('./config.js')
+const typeDefs = require('./graphql/typeDef')
+
+
+const server = new ApolloServer({
+    typeDefs,
+    resolvers
+});
+
+mongoose.connect(MONGODB, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => {
+        return server.listen({ port: 5500 })
+        console.log('MongoDB Connected')
+    })
+    .then((res) => {
+        console.log(`Server running at ${res.url}`)
+    })
+
+server.listen({ port: 5000 })
+    .then(res => {
+        console.log(`Server running at ${res.url}`)
+    })
